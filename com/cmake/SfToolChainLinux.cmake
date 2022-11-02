0145b3679c1a7d3a2c77e25839ab402da7a033c9
Arjan van Olphen <arjan@scanframe.nl>
2022-11-02 08:35:48 +0100

    Tool chain finds latest compiler for Linux.

--- com/cmake/SfToolChainLinux.cmake
+++ com/cmake/SfToolChainLinux.cmake
@@ -1,6 +1,24 @@
 # Notify that this file was loaded.
-message(STATUS "Configuring Linux for compiler version 12")
+message(STATUS "Configuring Linux for latest compiler version")
 
-# Use gcc-12 and g++-12 compilers.
-set(CMAKE_C_COMPILER /usr/bin/gcc-12)
-set(CMAKE_CXX_COMPILER /usr/bin/g++-12)
+# Dirty way of getting the newest installed C compiler on the system.
+foreach(_Version RANGE 14 8 -1)
+	find_program(_Compiler "/usr/bin/gcc-${_Version}")
+	if (NOT _Compiler STREQUAL "_Compiler-NOTFOUND")
+		message(STATUS "Found compiler: ${_Compiler}")
+		set(CMAKE_C_COMPILER "${_Compiler}")
+		break()
+	endif()
+endforeach()
+
+# Dirty way of getting the newest installed C++ compiler on the system.
+foreach(_Version RANGE 14 8 -1)
+	find_program(_Compiler "/usr/bin/g++-${_Version}")
+	if (NOT _Compiler STREQUAL "_Compiler-NOTFOUND")
+		message(STATUS "Found compiler: ${_Compiler}")
+		set(CMAKE_CXX_COMPILER "${_Compiler}")
+		break()
+	endif()
+endforeach()
+
+
